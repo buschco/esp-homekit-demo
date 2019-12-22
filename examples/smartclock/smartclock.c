67e070012377d9eafade2806db541c16ca4f88d0
buschco <colin@busch.dev>
2019-12-22 17:06:41 +0100

    Added smartclock

---
+++ examples/smartclock/smartclock.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <espressif/esp_wifi.h>
+#include <espressif/esp_sta.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include "wifi.h"
+#include <sntp.h>
+#include <time.h>
+
+#define SNTP_SERVERS 	"0.pool.ntp.org", "1.pool.ntp.org", \
+						"2.pool.ntp.org", "3.pool.ntp.org"
+
+#define vTaskDelayMs(ms)	vTaskDelay((ms)/portTICK_PERIOD_MS)
+#define UNUSED_ARG(x)	(void)x
+
+
+static void wifi_init() {
+    struct sdk_station_config wifi_config = {
+        .ssid = WIFI_SSID,
+        .password = WIFI_PASSWORD,
+    };
+
+    sdk_wifi_set_opmode(STATION_MODE);
+    sdk_wifi_station_set_config(&wifi_config);
+    sdk_wifi_station_connect();
+}
+
+const int led_gpio = 2;
+bool led_on = false;
+
+void led_write(bool on) {
+    gpio_write(led_gpio, on ? 0 : 1);
+}
+
+void led_init() {
+    gpio_enable(led_gpio, GPIO_OUTPUT);
+    led_write(led_on);
+}
+
+void led_identify_task(void *_args) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            led_write(true);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            led_write(false);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    led_write(led_on);
+
+    vTaskDelete(NULL);
+}
+
+void led_identify(homekit_value_t _value) {
+    printf("LED identify\n");
+    xTaskCreate(led_identify_task, "LED identify", 128, NULL, 2, NULL);
+}
+
+homekit_value_t led_on_get() {
+    return HOMEKIT_BOOL(led_on);
+}
+
+void led_on_set(homekit_value_t value) {
+    if (value.format != homekit_format_bool) {
+        printf("Invalid value format: %d\n", value.format);
+        return;
+    }
+
+    led_on = value.bool_value;
+    led_write(led_on);
+}
+
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]){
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HaPK"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "037A2BABF19D"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "MyLED"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, led_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Sample LED"),
+            HOMEKIT_CHARACTERISTIC(
+                ON, false,
+                .getter=led_on_get,
+                .setter=led_on_set
+            ),
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "111-11-111"
+};
+
+void sntp_tsk(void *pvParameters)
+{
+	const char *servers[] = {SNTP_SERVERS};
+	UNUSED_ARG(pvParameters);
+
+	/* Wait until we have joined AP and are assigned an IP */
+	while (sdk_wifi_station_get_connect_status() != STATION_GOT_IP) {
+		vTaskDelayMs(100);
+	}
+
+	/* Start SNTP */
+	printf("Starting SNTP... ");
+	/* SNTP will request an update each 5 minutes */
+	sntp_set_update_delay(5*60000);
+	/* Set GMT+1 zone, daylight savings off */
+	const struct timezone tz = {1*60, 0};
+	/* SNTP initialization */
+	sntp_initialize(&tz);
+	/* Servers must be configured right after initialization */
+	sntp_set_servers(servers, sizeof(servers) / sizeof(char*));
+	printf("DONE!\n");
+
+	/* Print date and time each 5 seconds */
+	while(1) {
+		vTaskDelayMs(5000);
+		time_t ts = time(NULL);
+		printf("TIME: %s", ctime(&ts));
+	}
+}
+
+void user_init(void) {
+    uart_set_baud(0, 115200);
+    xTaskCreate(sntp_tsk, "SNTP", 1024, NULL, 1, NULL);
+    wifi_init();
+    led_init();
+    homekit_server_init(&config);
+}
